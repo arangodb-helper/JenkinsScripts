b2acad275e4ca156944c4cb9c6d7a23f8ff5e278
Wilfried Goesgens <willi@arangodb.com>
2016-11-30 12:39:31 +0100

    comment out publish stage

--- enterprise/addCustomer.groovy
+++ enterprise/addCustomer.groovy
@@ -5,18 +5,18 @@ stage("addCustomer") {
   }
 }
 
-stage("publish customer to docker registry") {
-  node('master') {
-    build( job: 'RELEASE__UpdateDockerResources',
-           parameters: [
-             string(name: 'GITTAG', value: params['GITTAG']),
-             booleanParam(name: 'NEW_MAJOR_RELEASE', value: false),
-             booleanParam(name: 'UPDATE_MESOS_IMAGE', value: false),
-             booleanParam(name: 'UPDATE_UNOFFICIAL_IMAGE', value: false),
-             booleanParam(name: 'CREATE_DOCKER_LIBRARY_PULLREQ', value: false),
-             booleanParam(name: 'CREATE_NEW_VERSION', value: false),
-             booleanParam(name: 'DEBUG', value: false)
-           ]
-         )
-  }
-}
+//stage("publish customer to docker registry") {
+//  node('master') {
+//    build( job: 'RELEASE__UpdateDockerResources',
+//           parameters: [
+//             string(name: 'GITTAG', value: params['GITTAG']),
+//             booleanParam(name: 'NEW_MAJOR_RELEASE', value: false),
+//             booleanParam(name: 'UPDATE_MESOS_IMAGE', value: false),
+//             booleanParam(name: 'UPDATE_UNOFFICIAL_IMAGE', value: false),
+//             booleanParam(name: 'CREATE_DOCKER_LIBRARY_PULLREQ', value: false),
+//             booleanParam(name: 'CREATE_NEW_VERSION', value: false),
+//             booleanParam(name: 'DEBUG', value: false)
+//           ]
+//         )
+//  }
+//}
