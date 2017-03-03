fc4b81b75d049c22f4da646b41be6ac37d4b752c
Wilfried Goesgens <willi@arangodb.com>
2017-03-03 19:10:34 +0100

    brackets

--- release/Flow.groovy
+++ release/Flow.groovy
@@ -1,8 +1,8 @@
 def LinuxTargets
 def DOCKER_HOST_2
 def DOCKER_HOST="docker"
-echo params['HAVE_2_BUILDERS']
-if (params['HAVE_2_BUILDERS'] == "true") {
+echo "${params['HAVE_MORE_BUILDERS']}"
+if (params['HAVE_MORE_BUILDERS'] == "true") {
   echo "Have 2 docker hosts!"
   DOCKER_HOST_2='docker2'
   LinuxTargets="LinuxEnterprise"
@@ -46,7 +46,7 @@ stage("building packages") {
                )
 
           ///----------------------------------------------------------------------
-	  if (params['HAVE_2_BUILDERS'] == "false") {
+	  if (params['HAVE_MORE_BUILDERS'] == "false") {
             echo "building Linux Community Release"
             build( job: 'RELEASE__BuildPackages',
                    parameters: [
@@ -63,7 +63,7 @@ stage("building packages") {
         ////////////////////////////////////////////////////////////////////////////////
         "linuxCommunityPackages": {
           ///----------------------------------------------------------------------
-	  if (params['HAVE_2_BUILDERS'] == "true") {
+	  if (params['HAVE_MORE_BUILDERS'] == "true") {
             echo "building Linux Community Release"
             build( job: 'RELEASE__BuildPackages',
                    parameters: [
