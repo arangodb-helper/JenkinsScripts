6fb5594b73e4186d355c1ff057f8f4d33f0a3e12
Wilfried Goesgens <willi@arangodb.com>
2017-08-14 18:53:36 +0200

    security improvement - don't copy pre-installed files from the root home

--- release/BuildDocumentation.groovy
+++ release/BuildDocumentation.groovy
@@ -104,8 +104,8 @@ def compileSource(buildEnv, Boolean buildUnittestTarball, String enterpriseUrl, 
     ARANGODB_VERSION_MINOR=`grep 'set(ARANGODB_VERSION_MINOR' CMakeLists.txt | sed 's;.*\"\\(.*\\)\".*;\\1;'`
     ARANGODB_VERSION_REVISION=`grep 'set(ARANGODB_VERSION_REVISION' CMakeLists.txt | sed 's;.*\"\\(.*\\)\".*;\\1;'`
 
-    cp -a /root/.gitbook/ ~/
-    cp -a /root/.npm/ ~/
+    cp -a /tmp/.gitbook/ ~/
+    cp -a /tmp/.npm/ ~/
 
     /usr/local/bin/gitbook ls
     echo blarg
