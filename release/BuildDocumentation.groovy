3d3b588ce608ed2ce4321e95c442a137fcbb4b7e
Wilfried Goesgens <willi@arangodb.com>
2017-08-15 11:25:21 +0200

    debug

--- release/BuildDocumentation.groovy
+++ release/BuildDocumentation.groovy
@@ -104,6 +104,8 @@ def compileSource(buildEnv, Boolean buildUnittestTarball, String enterpriseUrl, 
     ARANGODB_VERSION_MINOR=`grep 'set(ARANGODB_VERSION_MINOR' CMakeLists.txt | sed 's;.*\"\\(.*\\)\".*;\\1;'`
     ARANGODB_VERSION_REVISION=`grep 'set(ARANGODB_VERSION_REVISION' CMakeLists.txt | sed 's;.*\"\\(.*\\)\".*;\\1;'`
 
+    ls -l /usr/local/ /usr/local/nodeshit/
+    ln -s /usr/local/nodeshit/gitbook ~/.gitbook
     ln -s /usr/local/nodeshit/gitbook ~/.gitbook
     ln -s /usr/local/nodeshit/npm ~/.npm
 
